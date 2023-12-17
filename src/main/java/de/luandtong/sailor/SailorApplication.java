364278213c04f1eaa335192feacc0e567a22009a
Tong <cnlutong@gmail.com>
2023-12-17 18:56:08 +0100

    creative repository

---
+++ src/main/java/de/luandtong/sailor/SailorApplication.java
@@ -0,0 +1,13 @@
+package de.luandtong.sailor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SailorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SailorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/luandtong/sailor/SailorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.luandtong.sailor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SailorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
