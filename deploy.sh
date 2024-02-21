e37c2539f371106256ab104fef0009a0adfaa04e
Tong <cnlutong@gmail.com>
2024-02-21 16:44:47 +0100

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -30,7 +30,7 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
 
 
 # 4. 下载 docker-compose.yml 文件
